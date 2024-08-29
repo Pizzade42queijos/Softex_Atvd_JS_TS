a42d7db189ef3d4368dfc896cae77fedf5ad3c4f
Audemário Alves <aamf@discente.ifpe.edu.br>
2024-08-29 13:25:21 -0300

    Update index4_typescript.ts

--- index4_typescript.ts
+++ index4_typescript.ts
@@ -1,21 +1,21 @@
-const prompt = require('prompt-sync')({sigint: true});
-const nota1 = Number(prompt("Digite a nota do primeiro aluno\n"))
-const nota2 = Number(prompt("Digite a nota do segundo aluno:\n"))
-const media = 6
-if((typeof nota1 == number)&&(typeof nota2 == number)){
-    if (nota1>=6){
-       console.log(`O primeiro aluno, que obteve a nota ${nota1}, está aprovado`);
+import readlineSync from 'readline-sync';
+
+const nota1: number = Number(readlineSync.question("Digite a nota do primeiro aluno:\n"));
+const nota2: number = Number(readlineSync.question("Digite a nota do segundo aluno:\n"));
+const media: number = 6;
+
+if (!isNaN(nota1) && !isNaN(nota2)) {
+    if (nota1 >= media) {
+        console.log(`O primeiro aluno, que obteve a nota ${nota1}, está aprovado.`);
+    } else {
+        console.log(`O primeiro aluno, que obteve a nota ${nota1}, está reprovado.`);
     }
-    else{
-        console.log(`O primeiro aluno, que obteve a nota ${nota1}, está reprovado`);
+
+    if (nota2 >= media) {
+        console.log(`O segundo aluno, que obteve a nota ${nota2}, está aprovado.`);
+    } else {
+        console.log(`O segundo aluno, que obteve a nota ${nota2}, está reprovado.`);
     }
-    if (nota2>=6){
-        console.log(`O segundo aluno, que obteve a nota ${nota2}, está aprovado`);
-    }
-    else{
-        console.log(`O segundo aluno, que obteve a nota ${nota2}, está reprovado`);
-    }
-}
-else{
-    console.log("Um valor inválido foi digitado, impossível concluir a operação")
+} else {
+    console.log("Um valor inválido foi digitado, impossível concluir a operação.");
 }
